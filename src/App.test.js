bdfe1a11caf3d3b619803cbc4375e145c7b97b03
Christian Mejia <chrismejia10@gmail.com>
2019-10-27 20:22:29 -0400

    pre-pre-eject

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
-import { exportAllDeclaration } from "@babel/types";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
